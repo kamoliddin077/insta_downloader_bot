aa9ed7e3d33302f2b97353777b55c14534e8340a
kamoliddin077 <yoshmusulmon@gmail.com>
2025-12-10 13:53:51 +0500

    Create bot.py

---
+++ bot.py
@@ -0,0 +1,26 @@
+import telebot
+import requests
+import os
+
+TOKEN = 8541191197:AAGGmf5zK7-yMDONae0m6joEm5jsU6b-D08
+
+bot = telebot.TeleBot(TOKEN)
+
+def download_insta(link):
+    api = f"https://api.oust.me/instagram?url={link}"
+    r = requests.get(api).json()
+    return r["url"]
+
+@bot.message_handler(commands=["start"])
+def start(message):
+    bot.reply_to(message, "Salom 👋\nMengaga Instagram video link yuboring 📥")
+
+@bot.message_handler(func=lambda message: True)
+def insta_down(message):
+    try:
+        url = download_insta(message.text)
+        bot.send_video(message.chat.id, url, caption="🎬 Yuklab olindi!")
+    except:
+        bot.reply_to(message, "❌ Xato link! Yana urinib ko‘ring.")
+
+bot.polling()
